45cf2367633da03e6982a946c219228d94bcef07
Tishalovich Maxim <tishalovichm@gmail.com>
2026-05-10 15:31:45 +0300

    Security config changes

--- src/main/java/by/tishalovichm/coffee/config/SecurityConfig.java
+++ src/main/java/by/tishalovichm/coffee/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import by.tishalovichm.coffee.auth.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,13 +18,12 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
         http
             .authorizeHttpRequests(req ->
-            req.requestMatchers(HttpMethod.GET, "/coffees").authenticated()
-                .requestMatchers( "/auth/sign-up", "/error").permitAll()
+            req.requestMatchers(HttpMethod.GET, "/api/coffees", "/api/coffees/*").permitAll()
+                .requestMatchers( "/api/auth/sign-up", "/api/coffees/proposal/json").permitAll()
                 .anyRequest().authenticated()
             );
 
         http.csrf(AbstractHttpConfigurer::disable);
-        http.formLogin(Customizer.withDefaults());
         http.httpBasic(httpBasic ->
             httpBasic.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         http.exceptionHandling(c ->
